d70fc29d563ce32ee495743bbb169c4dc25f030d
Szykuc2001 <76168295+Szykuc2001@users.noreply.github.com>
2021-10-08 16:52:19 +0200

    Zadanie 1 Szymon Kuczyński

---
+++ lab01/src/main/java/org/example/ObjectPropertyProvider.java
@@ -0,0 +1,55 @@
+package org.example;
+
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+
+public class ObjectPropertyProvider {
+
+    public List<Method> getPublicGetters(Class<?> clazz){
+        Method[] methods = clazz.getMethods();
+        List<Method> getters = null;
+
+        for (Method method : methods) {
+            if (method.getName().startsWith("get") || method.getName().startsWith("is")) {
+                if (Modifier.isPublic(method.getModifiers())) {
+                    if (method.getParameterTypes().length == 0) {
+                        assert false;
+                        getters.add(method);
+                    }
+                }
+            }
+        }
+        return getters;
+    }
+
+
+    public List<Method> getPublicSetters(Class<?> clazz){
+        Method[] methods = clazz.getMethods();
+        List<Method> setters = null;
+
+        for (Method method : methods) {
+            if (method.getName().startsWith("set")) {
+                if (Modifier.isPublic(method.getModifiers())) {
+                    if (method.getParameterTypes().length == 0) {
+                        assert false;
+                        setters.add(method);
+                    }
+                }
+            }
+        }
+        return setters;
+    }
+
+
+    public List<Field> getFieldsForPublicProperties(Class<?> clazz){
+
+        return Arrays.stream(clazz.getDeclaredFields()).toList();
+
+    }
+
+
+}

bc67181e9aa972ef47a65449264908f63bb969bf
Kacper Stankiewicz <s22619@pjwstk.edu.pl>
2021-10-08 16:56:36 +0200

    setters and getters

--- lab01/src/main/java/org/example/Application.java
+++ lab01/src/main/java/org/example/Application.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Class<sampleClass> clazz = sampleClass.class;
+        List<Field> fields = Arrays.stream(clazz.getDeclaredFields()).toList();
+
 
     }
 }
--- lab01/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,26 +3,59 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
-    public List<Method> getPublicGetters(Class<?> clazz){
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+    public List<Method> getPublicGetters(Class<?> clazz) {
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods()).toList();
+        List<Method> getters = new java.util.ArrayList<>(Collections.emptyList());
+
+
+        for (Method m : methods) {
+            String methodName = m.getName();
+            if (Modifier.isPublic(m.getModifiers())) {
+                if (methodName.startsWith("get") || methodName.startsWith("is")) {
+                    if (m.getParameterCount() == 0) {
+                        if(!void.class.equals(m.getReturnType())){
+                            getters.add(m);
+                        }
+                    }
+                }
+            }
+        }
+
+        return getters;
     }
 
 
-    public List<Method> getPublicSetters(Class<?> clazz){
+    public List<Method> getPublicSetters(Class<?> clazz) {
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods()).toList();
+        List<Method> setters = new java.util.ArrayList<>(Collections.emptyList());
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+
+        for (Method m : methods) {
+            String methodName = m.getName();
+            if (Modifier.isPublic(m.getModifiers())) {
+                if (methodName.startsWith("set")) {
+                    if (m.getParameterCount() == 1) {
+                        if(void.class.equals(m.getReturnType())){
+                            setters.add(m);
+                        }
+                    }
+                }
+            }
+        }
+
+        return setters;
     }
 
 
-    public List<Field> getFieldsForPublicProperties(Class<?> clazz){
-
+    public List<Field> getFieldsForPublicProperties(Class<?> clazz) {
         return Arrays.stream(clazz.getDeclaredFields()).toList();
-
     }
 
 

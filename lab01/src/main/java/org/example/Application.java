330cc5086d720cd9b7397288d3f2a648ae394364
s22432 <s22432@pjwstk.edu.pl>
2021-10-08 17:01:30 +0200

    public getters wip

--- lab01/src/main/java/org/example/Application.java
+++ lab01/src/main/java/org/example/Application.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.lang.reflect.Method;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args){
--- lab01/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,13 +3,28 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        List<Method> getters = new ArrayList<>();
+        Method[] methods = clazz.getMethods();
+
+        for(Method method : methods){
+            if(isGetter(method)){
+                getters.add(method);
+            };
+        }
+        return Arrays.stream();
+    }
+
+    public static boolean isGetter(Method method){
+        if(!method.getName().startsWith("get"))    return false;
+        if(method.getParameterTypes().length != 0)   return false;
+        return true;
     }
 
 

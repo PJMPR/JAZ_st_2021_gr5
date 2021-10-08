5e4c979de31858e715025e8781719f39232b367c
Julia Piecha <s22202@pjwstk.edu.pl>
2021-10-08 16:58:01 +0200

    Passed two tests, working on the third one

--- lab01/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/src/main/java/org/example/ObjectPropertyProvider.java
@@ -1,21 +1,55 @@
 package org.example;
 
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        ArrayList<Method> list = new ArrayList<Method>();
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods) {
+            if (isGetter(method)) {
+                list.add(method);
+            }
+        }
+        return list;
+    }
+
+    public static boolean isGetter(Method method) {
+        if (Modifier.isPublic(method.getModifiers()) &&
+                method.getParameterTypes().length == 0) {
+            if (method.getName().matches("^get[A-Z].*") &&
+                    !method.getReturnType().equals(void.class))
+                return true;
+            if (method.getName().matches("^is[A-Z].*") &&
+                    method.getReturnType().equals(boolean.class))
+                return true;
+        }
+        return false;
     }
 
 
     public List<Method> getPublicSetters(Class<?> clazz){
+        ArrayList<Method> list = new ArrayList<Method>();
+        Method[] methods = clazz.getDeclaredMethods();
+        for (Method method : methods)
+            if (isSetter(method))
+                list.add(method);
+        return list;
+    }
 
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+    public static boolean isSetter(Method method) {
+        return Modifier.isPublic(method.getModifiers()) &&
+                method.getReturnType().equals(void.class) &&
+                method.getParameterTypes().length == 1 &&
+                method.getName().matches("^set[A-Z].*");
     }
 
 

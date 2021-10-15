025b40cbef3efbe5ce8bdf1dee7d604f7807604f
adam urbanowicz <adam.urbanow@gmail.com>
2021-10-15 17:13:19 +0200

    lab01 solved

--- lab01/src/main/java/org/example/ObjectPropertyProvider.java
+++ lab01/src/main/java/org/example/ObjectPropertyProvider.java
@@ -3,27 +3,106 @@ package org.example;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.time.temporal.IsoFields;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 public class ObjectPropertyProvider {
 
     public List<Method> getPublicGetters(Class<?> clazz){
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods()).toList();
+        List<Method> result = new ArrayList<>();
+        MethodInspector inspector = new MethodInspector();
+
+        for (Method method : methods) {
+            inspector.setMethod(method);
+            if(inspector.isGettter())
+                result.add(method);
+        }
+        return result;
     }
 
 
     public List<Method> getPublicSetters(Class<?> clazz){
-
-        return Arrays.stream(clazz.getDeclaredMethods()).toList();
+        List<Method> methods = Arrays.stream(clazz.getDeclaredMethods()).toList();
+        List<Method> result = new ArrayList<>();
+        MethodInspector inspector = new MethodInspector();
+        for (Method method : methods) {
+            inspector.setMethod(method);
+            if(inspector.isSetter())
+                result.add(method);
+        }
+        return result;
     }
 
 
     public List<Field> getFieldsForPublicProperties(Class<?> clazz){
 
-        return Arrays.stream(clazz.getDeclaredFields()).toList();
+        List<Field> fields = Arrays.stream(clazz.getDeclaredFields()).toList();
+        List<Field> result = new ArrayList<>();
+        List<Method> methods = getPublicGetters(clazz);
+        methods.addAll(getPublicSetters(clazz));
 
+        MethodInspector inspector = new MethodInspector();
+        for (Method method :
+                methods) {
+            inspector.setMethod(method);
+            String fieldName = inspector.getFieldName();
+            for (Field field :
+                    fields) {
+             if(field.getName()==fieldName && !fields.contains(field))
+                 result.add(field);
+            }
+        }
+        return result;
     }
 
 
+    class MethodInspector{
+        Method method;
+        public void setMethod(Method method) {
+            this.method=method;
+        }
+
+        public boolean isPublic() {
+            return Modifier.isPublic(method.getModifiers());
+        }
+
+        public boolean startsWith(String prefix) {
+            return method.getName().startsWith(prefix);
+        }
+
+        public boolean isVoid() {
+            return method.getReturnType().equals(void.class);
+        }
+
+        public boolean hasOneParameter() {
+            return method.getParameterCount()==1;
+        }
+
+        public String getFieldName() {
+            return method.getName()
+                    .replace("get", "")
+                    .replace("set","")
+                    .toLowerCase(Locale.ROOT);
+        }
+
+        public boolean isGettter() {
+            return isPublic()
+                    && startsWith("get")
+                    && !isVoid()
+                    && !hasOneParameter();
+        }
+
+        public boolean isSetter(){
+            return isPublic()
+                    && startsWith("set")
+                    && isVoid()
+                    && hasOneParameter();
+        }
+    }
 }
--- lab02/demo/src/main/java/org/example/Application.java
+++ lab02/demo/src/main/java/org/example/Application.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import org.example.validators.Validator;
+
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
 
         System.out.println("adam@wp.pl".matches("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$"));
 
+        Sample obj = new Sample();
+        System.out.println(
+        Sample.class.getDeclaredField("name").get(obj)==null);
+
     }
 }
+
+class Sample{ String name;}

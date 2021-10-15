c69f2daced088ab86b68a1f8ec9683dfd838d82e
s22432 <s22432@pjwstk.edu.pl>
2021-10-15 16:46:08 +0200

    task in progress

--- lab02/demo/src/main/java/org/example/Application.java
+++ lab02/demo/src/main/java/org/example/Application.java
@@ -1,8 +1,7 @@
 package org.example;
 
 public class Application {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         System.out.println("adam@wp.pl".matches("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$"));
 
     }
--- lab02/demo/src/main/java/org/example/validators/Validator.java
+++ lab02/demo/src/main/java/org/example/validators/Validator.java
@@ -1,10 +1,21 @@
 package org.example.validators;
 
 
+import org.example.annotations.NotNull;
+
+import java.lang.reflect.Field;
+
 public class Validator {
 
-    public <TClass> ValidationResult validate(TClass object){
+    public <TClass> ValidationResult validate(TClass object) {
+        Field[] fields = object.getClass().getDeclaredFields();
 
-        return null;
+
+        for (int i = 0; i < fields.length; i++){
+            Field f = fields[i];
+            if (f.isAnnotationPresent(NotNull.class)){
+//                nullFields[i] = f.getAnnotation(NotNull.class).message();
+            }
+        }
     }
 }

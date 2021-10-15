877767a2f5d1de6035a8ac3b0e3ead4f9bb456ee
Kinga Labuda <s23457@pjwstk.edu.pl>
2021-10-15 16:47:57 +0200

    Added 3 annotations and slight changes to Validator class

--- lab02/demo/src/main/java/org/example/validators/Validator.java
+++ lab02/demo/src/main/java/org/example/validators/Validator.java
@@ -1,10 +1,31 @@
 package org.example.validators;
 
 
+import org.example.annotations.NotNull;
+import org.example.annotations.Range;
+import org.example.annotations.Regex;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Validator {
 
     public <TClass> ValidationResult validate(TClass object){
+        List<Field> declaredFields = Arrays.stream(object.getClass().getDeclaredFields()).toList();
+        List<String> annotationsList = Arrays.stream([NotNull, @Range, @Regex])
+        Map<String, List<Annotation>> objectsAnnotations = new HashMap<>();
+        for (Field field : declaredFields) {
+            for
+            if (field.isAnnotationPresent(NotNull.class)) {
+                objectsAnnotations.put(field.getName(), field.getAnnotation(NotNull.class));
+            }
 
-        return null;
+
+
+        }
     }
 }

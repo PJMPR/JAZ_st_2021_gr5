dcd5bd461160616fbd0f3d176d7cc0cb87d4e102
Kuba Nowacki <knowacki.main@gmail.com>
2021-10-16 21:46:04 +0200

    lab02/kuba nowacki/s22629/dev

---
+++ lab02/demo/src/main/java/org/example/annotations/NotNull.java
@@ -0,0 +1,12 @@
+package org.example.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotNull {
+    String errorMessage() default "field is null";
+}
---
+++ lab02/demo/src/main/java/org/example/annotations/Range.java
@@ -0,0 +1,14 @@
+package org.example.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Range {
+    int min();
+    int max();
+    String errorMessage() default "number is out of range";
+}

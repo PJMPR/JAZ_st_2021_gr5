ed980c24c2563a4e421f98ae4e5768153f85008e
1Artur125 <91741686+1Artur125@users.noreply.github.com>
2021-10-16 18:34:19 +0200

    dodaje adnotacje

---
+++ lab02/demo/src/main/java/org/example/annotations/Range.java
@@ -0,0 +1,15 @@
+package org.example.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface Range {
+    String msg() default "number is out of range [0,10]";
+    int min();
+    int max();
+}

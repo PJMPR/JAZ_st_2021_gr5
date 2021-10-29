be975afb2313e0a2744f44da27a009bbc5301ce9
mrizu <karol0704@wp.pl>
2021-10-29 16:57:40 +0200

    do domu

--- lab03/demo/src/main/java/org/example/queries/QueryProcessor.java
+++ lab03/demo/src/main/java/org/example/queries/QueryProcessor.java
@@ -5,10 +5,12 @@ import org.example.queries.results.Results;
 import org.example.queries.search.SearchParameters;
 
 public class QueryProcessor {
-
     public Results GetResults(SearchParameters parameters){
         Results result = new Results();
-
+        People.Data.stream()
+                .filter(age -> age.getAge()>=20 && age.getAge()<=40)
+                .filter(income -> income.getIncome()>=2000)
+                //.filter(page -> page.getSize)
         result.setItems(People.Data);
 
         return result;

d75b319a14ea0caeb22927e5326d998cb46e1e61
sugarvalley <cqr@o2.pl>
2021-11-08 01:14:01 +0100

    passing all tests

--- lab04/demo/src/main/java/org/example/caching/loaders/DictionaryCacheLoader.java
+++ lab04/demo/src/main/java/org/example/caching/loaders/DictionaryCacheLoader.java
@@ -1,6 +1,21 @@
 package org.example.caching.loaders;
 
-public class DictionaryCacheLoader {
+import org.example.caching.Cache;
+import org.example.caching.help.ItemsProvider;
 
-    public void load(){}
-}
+import java.util.Dictionary;
+import java.util.List;
+
+public class DictionaryCacheLoader implements CLoader{
+    public final Cache cache = Cache.getInstance();
+
+    public void load(){
+        ItemsProvider provider = new ItemsProvider();
+        List<Dictionary> records = provider.providedList();
+        String file = provider.file();
+        for (Dictionary record:
+                records) {
+            cache.add(file, record);
+        }
+        }
+    }

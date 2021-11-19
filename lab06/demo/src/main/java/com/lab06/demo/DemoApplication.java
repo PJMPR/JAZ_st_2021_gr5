19de980ae5f737f2687a8fb9e471a143dee7a24d
Wow0n <mati.maxx@o2.pl>
2021-11-19 19:18:21 +0100

    inital commit

---
+++ lab06/demo/src/main/java/com/lab06/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.lab06.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ lab06/demo/src/test/java/com/lab06/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab06.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

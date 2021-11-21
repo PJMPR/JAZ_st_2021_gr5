dab3f6c20a5a1ef77927fd0ee65a28140b4c7b47
KacperStankiewicz <s22619@pjwstk.edu.pl>
2021-11-21 16:59:58 +0100

    initial commit

---
+++ lab06/src/main/java/com/example/LoanScheduler/LoanSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoanScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanSchedulerApplication.class, args);
+	}
+
+}
---
+++ lab06/src/test/java/com/example/LoanScheduler/LoanSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoanScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

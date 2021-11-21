f657cf5b71f2e6ac9c83c402a632f111bae54a8b
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2021-11-21 22:11:19 +0100

    pdfFileWriter zoptymalizowany

--- lab06/demo/src/main/java/com/example/demo/fileWriter/PdfFileWriter.java
+++ lab06/demo/src/main/java/com/example/demo/fileWriter/PdfFileWriter.java
@@ -16,46 +16,11 @@ import java.util.List;
 
 @Component
 public class PdfFileWriter implements FileWriter{
-    @Override
-    public void getFile(HttpServletResponse response, int id, TimetableService timetableService) throws IOException {
-        Document document = new Document(PageSize.A4);
-        PdfWriter.getInstance(document, response.getOutputStream());
 
-        document.open();
-        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-        font.setSize(18);
-        font.setColor(Color.BLUE);
-
-        Paragraph p = new Paragraph("Timetable id="+id, font);
-        p.setAlignment(Paragraph.ALIGN_CENTER);
-
-        document.add(p);
-
-        PdfPTable table = new PdfPTable(6);
-        table.setWidthPercentage(100f);
-        table.setWidths(new float[] {2.0f, 3.0f, 3.0f, 3.0f, 3.0f,3.0f});
-        table.setSpacingBefore(10);
-
-        writeTableHeader(table);
-
-        List<Installment> installmentList= timetableService.findTimetableById(id).getInstalments();
-        for (Installment installment : installmentList) {
-            table.addCell(String.valueOf(installment.getId()));
-            table.addCell(String.valueOf(installment.getCapital()));
-            table.addCell(String.valueOf(installment.getCapitalToPay()));
-            table.addCell(String.valueOf(installment.getFixedFee()));
-            table.addCell(String.valueOf(installment.getInterest()));
-            table.addCell(String.valueOf(installment.getAmount()));
-        }
-
-        document.add(table);
-
-        document.close();
-    }
     private void writeTableHeader(PdfPTable table) {
         PdfPCell cell = new PdfPCell();
         cell.setBackgroundColor(Color.BLUE);
-        cell.setPadding(5);
+        cell.setPadding(7);
 
         Font font = FontFactory.getFont(FontFactory.HELVETICA);
         font.setColor(Color.WHITE);
@@ -80,12 +45,47 @@ public class PdfFileWriter implements FileWriter{
         table.addCell(cell);
     }
 
-    private void writeTableData(PdfPTable table) {
-
+    public void writeTableData(PdfPTable table,TimetableService timetableService ,int id){
+        List<Installment> installmentList= timetableService.findTimetableById(id).getInstalments();
+        for (Installment installment : installmentList) {
+            table.addCell(String.valueOf(installment.getId()));
+            table.addCell(String.valueOf(installment.getCapital()));
+            table.addCell(String.valueOf(installment.getCapitalToPay()));
+            table.addCell(String.valueOf(installment.getFixedFee()));
+            table.addCell(String.valueOf(installment.getInterest()));
+            table.addCell(String.valueOf(installment.getAmount()));
+        }
     }
 
-    public void export(HttpServletResponse response) throws DocumentException, IOException {
+
+    @Override
+    public void getFile(HttpServletResponse response, int id, TimetableService timetableService) throws IOException {
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, response.getOutputStream());
+
+        document.open();
+        Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        font.setSize(25);
+        font.setColor(Color.MAGENTA);
+
+        Paragraph p = new Paragraph("Timetable id="+id, font);
+        p.setAlignment(Paragraph.ALIGN_CENTER);
+
+        document.add(p);
+
+        PdfPTable table = new PdfPTable(6);
+        table.setWidthPercentage(100f);
+        table.setWidths(new float[] {2.0f, 3.0f, 3.0f, 3.0f, 3.0f,3.0f});
+        table.setSpacingBefore(10);
+
+        writeTableHeader(table);
+        writeTableData(table,timetableService,id);
 
 
+        document.add(table);
+
+        document.close();
     }
+
+
 }
--- lab06/demo/src/test/java/com/example/demo/InstallmentCalculatorTest.java
+++ lab06/demo/src/test/java/com/example/demo/InstallmentCalculatorTest.java
@@ -10,42 +10,40 @@ import java.util.List;
 
 public class InstallmentCalculatorTest {
     InstallmentCalculator installmentCalculator = new InstallmentCalculator();
+
     @Test
-    public void testIfCalculateInstalmentsReturnsEmptyListForTimetableWithInstallmentCountEqual0(){
-        Timetable timetable=new Timetable(1,100,0,0.1,100,"constant");
+    public void testIfCalculateInstalmentsReturnsEmptyListForTimetableWithInstallmentCountEqual0() {
+        Timetable timetable = new Timetable(1, 100, 0, 0.1, 100, "constant");
         List<Installment> result = installmentCalculator.calculateInstalments(timetable);
         Assertions.assertTrue(result.isEmpty());
     }
 
     @Test
-    public void testIfCalculateInstalmentsReturnsTimetableWith3elements(){
-        Timetable timetable=new Timetable(1,100,3,0.2,34,"decreasing");
+    public void testIfCalculateInstalmentsReturnsTimetableWith3elements() {
+        Timetable timetable = new Timetable(1, 100, 3, 0.2, 34, "decreasing");
         List<Installment> result = installmentCalculator.calculateInstalments(timetable);
-        Assertions.assertEquals(3,result.size());
+        Assertions.assertEquals(3, result.size());
     }
 
     @Test
-    public void testCalculateInterestForTypeConstant(){
-        Timetable timetable=new Timetable(1,100,5,0.1,20,"constant");
-        double interest= installmentCalculator.calculateInterest(timetable,1);
-        Assertions.assertEquals(2d,interest);
+    public void testCalculateInterestForTypeConstant() {
+        Timetable timetable = new Timetable(1, 100, 5, 0.1, 20, "constant");
+        double interest = installmentCalculator.calculateInterest(timetable, 1);
+        Assertions.assertEquals(2d, interest);
     }
 
     @Test
-    public void testCalculateInterestForTypeDecreasing(){
-        Timetable timetable=new Timetable(1,200,20,0.1,10,"decreasing");
-        double interest= installmentCalculator.calculateInterest(timetable,1);
-        Assertions.assertEquals(20d,interest);
+    public void testCalculateInterestForTypeDecreasing() {
+        Timetable timetable = new Timetable(1, 200, 20, 0.1, 10, "decreasing");
+        double interest = installmentCalculator.calculateInterest(timetable, 1);
+        Assertions.assertEquals(20d, interest);
     }
 
     @Test
-    public void testCalculateInterestForUnknownType(){
-        Timetable timetable=new Timetable(1,100,10,0.1,10,"unknown");
-        double interest= installmentCalculator.calculateInterest(timetable,1);
-        Assertions.assertEquals(0,interest);
+    public void testCalculateInterestForUnknownType() {
+        Timetable timetable = new Timetable(1, 100, 10, 0.1, 10, "unknown");
+        double interest = installmentCalculator.calculateInterest(timetable, 1);
+        Assertions.assertEquals(0, interest);
     }
-
-
-
 
 }

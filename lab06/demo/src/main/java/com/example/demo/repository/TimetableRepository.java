953916eeae1156002e60ab36c509c3f58de31a13
Jan Ostrowski <jan.ostrowski1337@gmail.com>
2021-11-22 17:14:07 +0100

    final commit

--- lab06/demo/src/main/java/com/example/demo/repository/TimetableRepository.java
+++ lab06/demo/src/main/java/com/example/demo/repository/TimetableRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.timetable.Timetable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TimetableRepository extends CrudRepository<Timetable, Integer> {
     public Timetable findById(int id);
 }

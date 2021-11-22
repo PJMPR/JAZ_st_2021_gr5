1ceba011cb32a4d267891fb3922a0d68d5d40386
Wow0n <mati.maxx@o2.pl>
2021-11-22 08:14:31 +0100

    tests done

--- lab06/demo/src/test/java/com/lab06/demo/CreditDecreasingTest.java
+++ lab06/demo/src/test/java/com/lab06/demo/CreditDecreasingTest.java
@@ -1,6 +1,6 @@
 package com.lab06.demo;
 
-import com.lab06.demo.creditcalculators.CreditConstant;
+import com.lab06.demo.creditcalculators.CreditDecreasing;
 import com.lab06.demo.entities.Calculation;
 import com.lab06.demo.entities.InstallmentType;
 import com.lab06.demo.entities.Timetable;
@@ -11,20 +11,22 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class CreditConstantTest {
+public class CreditDecreasingTest {
     @Test
     public void creditConstantShouldReturnProperValuesInList() {
         int amount = 300000;
         int installmentCount = 300;
-        InstallmentType installmentType = InstallmentType.CONSTANT;
+        InstallmentType installmentType = InstallmentType.DECREASING;
         double percentage = 0.047;
         int fixedRate = 30;
 
         Calculation calculation = new Calculation(amount, installmentCount, installmentType, percentage, fixedRate);
-        List<Timetable> actual = new CreditConstant(calculation).constantRateCalculation();
+        List<Timetable> actual = new CreditDecreasing(calculation).decreasingRateCalculation();
 
         assertThat(actual.size(), is(300));
-        assertThat(actual.get(299).getInterest(), is(703.35));
-        assertThat(actual.get(299).getAmount(), is(1733.35));
+        assertThat(actual.get(0).getInterest(), is(1175.0));
+        assertThat(actual.get(299).getInterest(), is(3.92));
+        assertThat(actual.get(0).getAmount(), is(1205.0));
+        assertThat(actual.get(299).getAmount(), is(33.92));
     }
 }

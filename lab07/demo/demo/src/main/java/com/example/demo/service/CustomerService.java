f4d947a6638da011472c0b6128dc7d4953353fe1
KacperStankiewicz <s22619@pjwstk.edu.pl>
2021-12-02 11:18:24 +0100

    charts added

--- lab07/demo/demo/src/main/java/com/example/demo/service/CustomerService.java
+++ lab07/demo/demo/src/main/java/com/example/demo/service/CustomerService.java
@@ -37,17 +37,12 @@ public class CustomerService {
     public List<customerSpentMoney> rankCustomersByMoneySpent() {
         Map<Integer, BigDecimal> myMap = new HashMap<>();
         Map<Integer, BigDecimal> finalMyMap = myMap;
-        //  List<Integer> ids = repository.findAll().stream().map(Customer::getCustomerId).collect(Collectors.toList());
-      //  for (int id : ids) {
-           // Map<Integer, BigDecimal> finalMyMap1 = myMap;
-            repository.findAll().stream().map(Customer::getCustomerId).forEach(id-> finalMyMap.put(id, repository.getById(id).getPayments().stream().map(Payment::getAmount).reduce(BigDecimal.valueOf(0), BigDecimal::add)));
-            //myMap.put(id, repository.getById(id).getPayments().stream().map(Payment::getAmount).reduce(BigDecimal.valueOf(0), BigDecimal::add));
-      //  }
+        repository.findAll().stream().map(Customer::getCustomerId).forEach(id -> finalMyMap.put(id, repository.getById(id).getPayments().stream().map(Payment::getAmount).reduce(BigDecimal.valueOf(0), BigDecimal::add)));
+
         myMap = myMap.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
-        //Map<Integer, BigDecimal> finalMyMap = myMap;
         myMap.keySet().stream().limit(10).forEach(key -> ranking.getRanking()
                 .add(new customerSpentMoney(repository.getById(key).getCustomerId(),
                         repository.getById(key).getFirstName(),
@@ -59,16 +54,16 @@ public class CustomerService {
 
     public List<customerWatchedMovies> rankCustomersByWatchedMovies() {
         Map<Integer, Integer> myMap = new HashMap<>();
-          List<Integer> ids = repository.findAll().stream().map(Customer::getCustomerId).collect(Collectors.toList());
-        for (int id : ids) {
-            myMap.put(id, repository.getById(id).getRentalsByCustomer().size());
-        }
+        //List<Integer> ids = repository.findAll().stream().map(Customer::getCustomerId).collect(Collectors.toList());
+
+        Map<Integer, Integer> finalMyMap = myMap;
+        repository.findAll().stream().map(Customer::getCustomerId).forEach(id-> finalMyMap.put(id, repository.getById(id).getRentalsByCustomer().size()));
+
 
         myMap = myMap.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
-        Map<Integer, Integer> finalMyMap = myMap;
         myMap.keySet().stream().limit(10)
                 .forEach(key -> ranking.getRankingMovies()
                         .add(new customerWatchedMovies(repository.getById(key).getCustomerId(),
@@ -79,17 +74,17 @@ public class CustomerService {
     }
 
     public List<rentMoviesByMonth> rentMoviesByMonths(int year) {
-           Map<Integer, Integer> myMap = new HashMap<>();
-           List<Integer> ids = rentalRepository.findAll().stream().map(Rental::getRentalId).collect(Collectors.toList());
+        Map<Integer, Integer> myMap = new HashMap<>();
+        List<Integer> ids = rentalRepository.findAll().stream().map(Rental::getRentalId).collect(Collectors.toList());
 
         for (int id : ids) {
-            if (rentalRepository.findById(id).get().getRentalDate().getYear() == 2005) {
+           // if (rentalRepository.findById(id).get().getRentalDate().getYear() == 2005) {
                 if (!myMap.containsKey(rentalRepository.findById(id).get().getRentalDate().getMonth())) {
                     myMap.put(rentalRepository.findById(id).get().getRentalDate().getMonth(), 1);
                 } else {
                     myMap.put(rentalRepository.findById(id).get().getRentalDate().getMonth(), myMap.get(rentalRepository.findById(id).get().getRentalDate().getMonth()) + 1);
                 }
-            }
+           // }
         }
         myMap.keySet()
                 .forEach(key -> ranking.getRentMoviesByMonths()

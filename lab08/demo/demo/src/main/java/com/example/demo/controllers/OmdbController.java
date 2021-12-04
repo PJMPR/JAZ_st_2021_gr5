5d2eb6beb5d52bfd73363692b8cfea48dcb9f7ad
MriZu <karol0704@wp.pl>
2021-12-04 18:16:26 +0100

    connected to db and APIs

--- lab08/demo/demo/src/main/java/com/example/demo/controllers/OmdbController.java
+++ lab08/demo/demo/src/main/java/com/example/demo/controllers/OmdbController.java
@@ -2,29 +2,24 @@ package com.example.demo.controllers;
 
 import com.example.demo.contract.MovieDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
-@Controller
-@RequestMapping("moviesclient")
-public class TheMovieDbController {
+public class OmdbController {
 
     RestTemplate rest;
 
-    public TheMovieDbController(RestTemplate rest) {
+    public OmdbController(RestTemplate rest) {
         this.rest = rest;
     }
 
     @GetMapping
     @RequestMapping("{id}")
     public ResponseEntity getData(@PathVariable("id") int id){
-        var movie = rest.getForEntity("https://api.themoviedb.org/3/movie/" +
-                id +
-                "?api_key=" + System.getenv("TheMovieDbApiKey"), MovieDto.class).getBody();
+        var movie = rest.getForEntity("http://www.omdbapi.com/?apikey=" +
+                System.getenv("OmdbApiKey") + "&", MovieDto.class).getBody();
         return ResponseEntity.ok(movie);
     }
-
 }

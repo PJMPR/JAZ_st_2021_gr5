f26fac0edfc3612130a48f08f8fc19e926558b27
KacperStankiewicz <s22619@pjwstk.edu.pl>
2021-12-09 13:43:34 +0100

    Spring problem

--- lab08/demo/demo/src/main/java/com/example/demo/FilmService/FilmService.java
+++ lab08/demo/demo/src/main/java/com/example/demo/FilmService/FilmService.java
@@ -7,17 +7,19 @@ import com.example.demo.repository.ActorRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-@Component
+@Service
 public class FilmService {
     RestTemplate rest;
     ActorRepository actorRepository;
 
     @Autowired
-    public FilmService(RestTemplate rest, ActorRepository actorRepository) {
+    public FilmService(RestTemplate rest,ActorRepository actorRepository) {
         this.rest = rest;
         this.actorRepository = actorRepository;
     }
@@ -35,13 +37,13 @@ public class FilmService {
             List<Actor> actors = new ArrayList<>();
             String[] names = movieIMDB.getActors().split(",");
             for (String s : names) {
-             //   int actorId = checkIfActorExist(s);
-           //     if(actorId == 0){
+                int actorId = checkIfActorExist(s);
+                if(actorId == 0){
                     String[] parts = s.strip().split(" ");
                     actors.add(new Actor(parts[0],parts[1]));
-          //      }else {
-           //         actors.add(actorRepository.findById(actorId).get());
-           //     }
+                }else {
+                    actors.add(actorRepository.findById(actorId).get());
+                }
             }
             movie.setActors(actors);
         }
@@ -56,19 +58,19 @@ public class FilmService {
                 "&i=" + id, IMDBMovieDto.class).getBody();
     }
 
-//    public int checkIfActorExist(String name){
-//
-//        String firstname = name.strip().split(" ")[0];
-//        String lastname = name.strip().split(" ")[1];
-//        boolean isInDB= actorRepository.findAll().stream().anyMatch(actor -> Objects.equals(actor.getFirstName(), firstname)
-//                && Objects.equals(actor.getLastName(), lastname));
-//
-//        if(isInDB){
-//            return actorRepository.findAll().stream().filter(actor -> Objects.equals(actor.getFirstName(), firstname)
-//                    && Objects.equals(actor.getLastName(), lastname)).findFirst().get().getActorId();
-//        }else return 0;
-//
-//    }
+    public int checkIfActorExist(String name){
+
+        String firstname = name.strip().split(" ")[0];
+        String lastname = name.strip().split(" ")[1];
+        boolean isInDB= actorRepository.findAll().stream().anyMatch(actor -> Objects.equals(actor.getFirstName(), firstname)
+                && Objects.equals(actor.getLastName(), lastname));
+
+        if(isInDB){
+            return actorRepository.findAll().stream().filter(actor -> Objects.equals(actor.getFirstName(), firstname)
+                    && Objects.equals(actor.getLastName(), lastname)).findFirst().get().getActorId();
+        }else return 0;
+
+    }
 
 //    public ActorsDto getActorsInfoFromIMDB(String id) {
 //
--- lab08/demo/demo/src/main/java/com/example/demo/repository/ActorRepository.java
+++ lab08/demo/demo/src/main/java/com/example/demo/repository/ActorRepository.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ActorRepository extends JpaRepository<Actor,Integer> {
 }

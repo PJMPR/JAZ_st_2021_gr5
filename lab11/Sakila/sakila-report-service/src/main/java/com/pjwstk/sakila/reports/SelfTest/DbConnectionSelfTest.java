d9b92304f3b3ac9a830e3f8234c922315b0501d1
KacperStankiewicz <s22619@pjwstk.edu.pl>
2022-01-15 21:51:48 +0100

    db connection selfTest added

---
+++ lab11/Sakila/sakila-report-service/src/main/java/com/pjwstk/sakila/reports/SelfTest/DbConnectionSelfTest.java
@@ -0,0 +1,37 @@
+package com.pjwstk.sakila.reports.SelfTest;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class DbConnectionSelfTest implements iSelfTest {
+
+    private final EntityManager entityManager;
+
+    @Override
+    public SelfTestResult run() {
+        SelfTestResult result;
+        try {
+            entityManager.createNativeQuery("select 1 from film limit 1").getSingleResult();
+            result = SelfTestResult.builder()
+                    .name("Database connection test")
+                    .description("Checks if there is a connection with database")
+                    .passed(true)
+                    .errors(null)
+                    .build();
+        }catch (Exception e){
+            result = SelfTestResult.builder()
+                    .name("Database connection test")
+                    .description("Checks if there is a connection with database")
+                    .passed(false)
+                    .errors(List.of("No connection with databse",e.getMessage()))
+                    .build();
+        }
+        return result;
+    }
+}

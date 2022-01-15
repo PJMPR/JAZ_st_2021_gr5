5ab008ae816c78bb1c80512606f62a3b5bb41bcf
KacperStankiewicz <s22619@pjwstk.edu.pl>
2022-01-15 21:44:20 +0100

    SelfTest for connection with themoviedb added, SelfTest for connection with omdbapi added

---
+++ lab11/Sakila/sakila-films-updater/src/main/java/com/pjwstk/sakila/filmsupdater/selfTest/OMDBApiConnectionSelfTest.java
@@ -0,0 +1,48 @@
+package com.pjwstk.sakila.filmsupdater.selfTest;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class OMDBApiConnectionSelfTest implements iSelfTest{
+
+    RestTemplate rest;
+
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+
+    @Override
+    public SelfTestResult run(){
+        SelfTestResult result;
+        try{
+            var response = rest.getForEntity("https://www.omdbapi.com/?i=tt3896198&apikey="
+                    + System.getenv("OMDBapiKey"), String.class).getBody();
+
+            if(!response.isBlank() && !response.isEmpty() && response != null){
+                result = SelfTestResult.builder()
+                        .name("OMDB api connection test")
+                        .description("Checks if there is a connection with omdbapi.com")
+                        .passed(true)
+                        .errors(null)
+                        .build();
+            }else {
+                throw new Exception();
+            }
+        }catch (Exception e){
+            result = SelfTestResult.builder()
+                    .name("OMDB api connection test")
+                    .description("Checks if there is a connection with omdbapi.com")
+                    .passed(false)
+                    .errors(List.of("There is no connection with OMDB Api. Check your internet connection",e.getMessage()))
+                    .build();
+        }
+        return result;
+    }
+}
---
+++ lab11/Sakila/sakila-films-updater/src/main/java/com/pjwstk/sakila/filmsupdater/selfTest/TheMovieDBConnectionSelfTest.java
@@ -0,0 +1,56 @@
+package com.pjwstk.sakila.filmsupdater.selfTest;
+
+import com.pjwstk.sakila.diagnostics.*;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.InetAddress;
+import java.net.URL;
+import java.util.List;
+
+
+@Component
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class TheMovieDBConnectionSelfTest implements iSelfTest {
+
+    RestTemplate rest;
+
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
+    }
+
+    @Override
+    public SelfTestResult run() {
+        SelfTestResult result;
+        try {
+            var response = rest.getForEntity("https://api.themoviedb.org/3/movie/1"+
+                    "?api_key=" + System.getenv("TheMovieDbApiKey"),String.class).getBody();
+            if(!response.isBlank() && !response.isEmpty() && response != null){
+            result = SelfTestResult.builder()
+                    .name("TheMovieDB api connection test")
+                    .description("Checks if there is a connection with api.themoviedb.org")
+                    .passed(true)
+                    .errors(null)
+                    .build();
+            }else {
+                throw new Exception();
+            }
+        }catch (Exception e){
+            result = SelfTestResult.builder()
+                    .name("TheMovieDB api connection test")
+                    .description("Checks if there is a connection with api.themoviedb.org")
+                    .passed(false)
+                    .errors(List.of("No connection with TheMovieDB API. Check your internet connection",e.getMessage()))
+                    .build();
+        }
+        return result;
+    }
+}
